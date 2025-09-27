0ab3cfb25e10a0b65eb774873e72df9a190f8c08
Durvesh Patil <dspatil@gmail.com>
2025-09-27 12:12:32 +0530

    sections remove update

--- src/app/(frontend)/v3/page.tsx
+++ src/app/(frontend)/v3/page.tsx
@@ -10,8 +10,8 @@ import V3GoogleReviewsProof from './components/V3GoogleReviewsProof'
 import V3WebinarFocusedCTA from './components/V3WebinarFocusedCTA'
 import V3InstructorCredibility from './components/V3InstructorCredibility'
 import V3CourseValueProposition from './components/V3CourseValueProposition'
-import V3StudentResults from './components/V3StudentResults'
-import V3SmartInvestorsShowcase from './components/V3SmartInvestorsShowcase'
+// import V3StudentResults from './components/V3StudentResults'
+// import V3SmartInvestorsShowcase from './components/V3SmartInvestorsShowcase'
 import V3FrequentlyAskedQuestions from './components/V3FrequentlyAskedQuestions'
 import V3FinalConversionCTA from './components/V3FinalConversionCTA'
 
@@ -19,32 +19,32 @@ export default function V3HomePage() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      
+
       <main>
         {/* 1. Hero Section - Trust-First with Google Reviews */}
         <V3HeroTrustFirst />
-        
+
         {/* 2. Google Reviews Social Proof */}
         <V3GoogleReviewsProof />
-        
+
         {/* 3. Webinar-Focused Primary CTA */}
         <V3WebinarFocusedCTA />
-        
+
         {/* 4. Instructor Credibility */}
         <V3InstructorCredibility />
-        
+
         {/* 5. Course Value Proposition */}
         <V3CourseValueProposition />
-        
+
         {/* 6. Student Results & Success Stories */}
-        <V3StudentResults />
-        
+        {/* <V3StudentResults /> */}
+
         {/* 7. Smart Investors Showcase with Live PnL */}
-        <V3SmartInvestorsShowcase />
-        
+        {/* <V3SmartInvestorsShowcase /> */}
+
         {/* 8. Frequently Asked Questions */}
         <V3FrequentlyAskedQuestions />
-        
+
         {/* 9. Final Conversion CTA */}
         <V3FinalConversionCTA />
       </main>
