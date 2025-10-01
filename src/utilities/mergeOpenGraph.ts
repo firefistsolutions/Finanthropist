60348aab22c6e0b119c2f7b512fc1fce8dcaac0c
Durvesh Patil <dspatil@gmail.com>
2025-10-01 21:55:08 +0530

    Logo Update

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -4,11 +4,11 @@ import { getServerSideURL } from './getURL'
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
   description: 'Finanthropist - Empowering your financial journey.',
-  images: [
-    {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
-    },
-  ],
+  // images: [
+  //   {
+  //     url: `${getServerSideURL()}/website-template-OG.webp`,
+  //   },
+  // ],
   siteName: 'Finanthropist',
   title: 'Finanthropist',
 }
